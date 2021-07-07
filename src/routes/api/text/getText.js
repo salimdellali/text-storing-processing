cea5a9ef60ac1589a269908ce0e6474b167c188f
Salim DELLALI <dellali.salim@gmail.com>
2021-07-07 18:01:13 +0100

    added pagination support to GET text route

--- src/routes/api/text/getText.js
+++ src/routes/api/text/getText.js
@@ -1,17 +1,27 @@
 const express = require('express');
+const buildPaginator = require('pagination-apis');
 const router = express.Router();
 const Text = require('../../../models/Text');
 
 /**
- * @route	GET api/attendees
- * @desc	Get all attendees
+ * @route	GET /text
+ * @desc	Fetch list of text with the support of pagination
  * @access	Public
  */
-router.get('/', (req, res) => {
+router.get('/', async (req, res) => {
+	const count = await Text.countDocuments();
+
 	Text.find()
 		.exec()
 		.then((texts) => {
-			res.json(texts);
+			const options = setOptions({ req, count });
+			const { paginate } = buildPaginator(options);
+			const concernedTexts = sliceTexts({
+				page: options.page,
+				limit: options.limit,
+				texts,
+			});
+			res.json(paginate(concernedTexts, count));
 		})
 		.catch((err) => {
 			res.json({ message: 'error has occured : ' + err });
@@ -19,3 +29,21 @@ router.get('/', (req, res) => {
 });
 
 module.exports = router;
+
+function setOptions({ req, count }) {
+	return {
+		limit: req.query.limit
+			? !isNaN(req.query.limit)
+				? req.query.limit
+				: count
+			: count,
+		page: req.query.page ? (!isNaN(req.query.page) ? req.query.page : 1) : 1,
+		url: `${req.protocol}://${req.get('host')}/text`,
+	};
+}
+
+function sliceTexts({ page, limit, texts }) {
+	const startIndex = (page - 1) * limit;
+	const endIndex = page * limit;
+	return texts.slice(startIndex, endIndex);
+}
